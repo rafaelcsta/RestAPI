01234943a434346a1dc7f17435fa5fb343e81db2
BlackCyber21 <rafael_for.work@hotmail.com>
2021-03-31 03:00:47 -0300

    RestAPI Simples

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/<int:id>")
+def pessoa(id):
+    soma = 1 + id
+    return jsonify({'id':id, 'nome':'Rafael', 'profissao':'Desenvolvedor'})
+
+#ADD FUNC SOMA VIA GET
+#@app.route('/soma/<int:valor1>/<int:valor2>/')
+#def soma(valor1, valor2):
+#    return jsonify({'soma':valor1 + valor2})
+
+#ADD FUNC SOMA VIA POST
+@app.route('/soma', methods=['POST', 'PUT', 'GET'])
+def soma():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        total = sum(dados['valores'])
+    elif request.method == 'GET':
+        total = 10 + 10
+    return jsonify({'soma':total})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
